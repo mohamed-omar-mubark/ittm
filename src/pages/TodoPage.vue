fceb847277f298fc1670b89019da227fafd97b6a
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2022-07-20 22:57:38 +0200

    feat: Put task into child component

--- src/pages/TodoPage.vue
+++ src/pages/TodoPage.vue
@@ -3,47 +3,11 @@
     <q-list
       separator
       bordered>
-      <q-item
-        v-for="(task, key) in tasks"
-        :key="key"
-        @click="task.completed = !task.completed"
-        :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
-        clickable
-        v-ripple>
-        <q-item-section side top>
-          <q-checkbox v-model="task.completed" />
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label
-            :class="{ 'text-strikethrough' : task.completed }">
-            {{ task.name }}
-          </q-item-label>
-        </q-item-section>
-
-        <q-item-section side>
-          <div class="row">
-            <div class="column justify-center">
-              <q-icon
-                name="event"
-                size="18px"
-                class="q-mr-xs" />
-            </div>
-            <div class="column">
-              <q-item-label
-                class="row justify-end"
-                caption>
-                {{ task.dueDate }}
-              </q-item-label>
-              <q-item-label
-                class="row justify-end"
-                caption>
-                <small>{{ task.dueTime }}</small>
-              </q-item-label>
-            </div>
-          </div>
-        </q-item-section>
-      </q-item>
+        <task
+          v-for="(task, key) in tasks"
+          :key="key"
+          :task="task"
+          :id="key"></task>
     </q-list>
   </q-page>
 </template>
@@ -51,9 +15,13 @@
 <script>
 // import mapGetters
 import { mapGetters } from 'vuex';
+
 export default {
   computed: {
     ...mapGetters('tasks', ['tasks'])
+  },
+  components: {
+    'task': require('../components/tasks/Task.vue').default,
   }
 }
 </script>
