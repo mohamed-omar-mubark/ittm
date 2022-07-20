ab710371fd55dc615bcb11c501406c11b3c95cd3
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2022-07-20 22:21:04 +0200

    feat: Add tasks dat to the vuex store and vuedevtools chroome

--- src/store/store-tasks.js
+++ src/store/store-tasks.js
@@ -1,5 +1,27 @@
 const state = {
-
+  tasks: [
+    {
+      id: 1,
+      name: 'Go to shop',
+      completed: false,
+      dueDate: '2022-08-01',
+      dueTime: '02:30'
+    },
+    {
+      id: 2,
+      name: 'Get bananas',
+      completed: false,
+      dueDate: '2022-08-02',
+      dueTime: '03:00'
+    },
+    {
+      id: 3,
+      name: 'Get apple',
+      completed: false,
+      dueDate: '2022-08-03',
+      dueTime: '05:45'
+    },
+  ]
 }
 
 const mutations = {
