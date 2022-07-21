6c7a795e66da4064a4fed40ae8507b51a003f749
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2022-07-21 18:46:46 +0200

    feat: Adding form validation and submition to add task inputs

--- src/components/tasks/modals/AddTask.vue
+++ src/components/tasks/modals/AddTask.vue
@@ -11,62 +11,66 @@
         icon="close" />
     </q-card-section>
 
-    <q-card-section>
-      <div class="row q-mb-sm">
-        <q-input
-          outlined
-          v-model="taskData.name"
-          label="Task name"
-          class="col"
-        ></q-input>
-      </div>
+    <q-form @submit.prevent="addTask">
+      <q-card-section>
+        <div class="row q-mb-sm">
+          <q-input
+            outlined
+            v-model="taskData.name"
+            :rules="[val => !!val || 'Field is required']"
+            ref="name"
+            label="Task name"
+            class="col"
+          ></q-input>
+        </div>
 
-      <div class="row q-mb-sm">
-        <q-input
-          outlined
-          label="Due date"
-          v-model="taskData.dueDate">
-          <template v-slot:append>
-            <q-icon name="event" class="cursor-pointer">
-              <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                <q-date v-model="taskData.dueDate">
-                  <div class="row items-center justify-end">
-                    <q-btn v-close-popup label="Close" color="primary" flat />
-                  </div>
-                </q-date>
-              </q-popup-proxy>
-            </q-icon>
-          </template>
-        </q-input>
-      </div>
+        <div class="row q-mb-sm">
+          <q-input
+            outlined
+            label="Due date"
+            v-model="taskData.dueDate">
+            <template v-slot:append>
+              <q-icon name="event" class="cursor-pointer">
+                <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                  <q-date v-model="taskData.dueDate">
+                    <div class="row items-center justify-end">
+                      <q-btn v-close-popup label="Close" color="primary" flat />
+                    </div>
+                  </q-date>
+                </q-popup-proxy>
+              </q-icon>
+            </template>
+          </q-input>
+        </div>
 
-      <div class="row q-mb-sm">
-        <q-input
-          outlined
-          label="Due time"
-          v-model="taskData.dueTime">
-          <template v-slot:append>
-            <q-icon name="access_time" class="cursor-pointer">
-              <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                <q-time v-model="taskData.dueTime">
-                  <div class="row items-center justify-end">
-                    <q-btn v-close-popup label="Close" color="primary" flat />
-                  </div>
-                </q-time>
-              </q-popup-proxy>
-            </q-icon>
-          </template>
-        </q-input>
-      </div>
-    </q-card-section>
+        <div class="row q-mb-sm">
+          <q-input
+            outlined
+            label="Due time"
+            v-model="taskData.dueTime">
+            <template v-slot:append>
+              <q-icon name="access_time" class="cursor-pointer">
+                <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                  <q-time v-model="taskData.dueTime">
+                    <div class="row items-center justify-end">
+                      <q-btn v-close-popup label="Close" color="primary" flat />
+                    </div>
+                  </q-time>
+                </q-popup-proxy>
+              </q-icon>
+            </template>
+          </q-input>
+        </div>
+      </q-card-section>
 
-    <q-card-actions align="right">
-      <q-btn
-        flat
-        label="Save"
-        color="primary"
-        v-close-popup />
-    </q-card-actions>
+      <q-card-actions align="right">
+        <q-btn
+          flat
+          label="Save"
+          color="primary"
+          type="submit" />
+      </q-card-actions>
+    </q-form>
   </q-card>
 </template>
 
@@ -82,6 +86,19 @@ export default {
       }
     };
   },
+  methods: {
+    // Add new task
+    addTask() {
+      // this.$refs.name.validate();
+      if(!this.$refs.name.hasError) {
+        this.submitTask();
+      }
+    },
+    // Submit task
+    submitTask() {
+      console.log('submitTask');
+    }
+  }
 };
 </script>
 
