f7254e7f2d73b2bd6055dd84c82c1e53c1b77160
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2022-07-22 13:41:48 +0200

    feat: Use computed property setter to set the vuex search

--- src/components/tasks/tools/Search.vue
+++ src/components/tasks/tools/Search.vue
@@ -19,8 +19,13 @@ import { mapState } from 'vuex'
 export default {
   computed: {
     ...mapState('tasks', ['search']),
-    searchField() {
-      return this.search
+    searchField: {
+      get(){
+        return this.search
+      },
+      set(value){
+        console.log('set', value);
+      }
     }
   },
 };
