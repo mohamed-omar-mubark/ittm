ea369f7b01b79a90aa30cc0c57c41b9e6080d3bf
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2022-07-23 18:19:09 +0200

    fix: Fix import firebase

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,6 +1,5 @@
-import firebase from 'firebase/app'
-
-import 'firebase/auth'
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,6 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-let firebaseAuth = app.auth()
+const firebaseAuth = getAuth(app)
 
 export { firebaseAuth }
