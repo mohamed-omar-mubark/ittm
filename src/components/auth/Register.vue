230172794e33c96caa8ecb20f99457585285d93d
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2022-07-23 15:36:53 +0200

    feat: Register: Add email and password fields and register button

--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -1,15 +1,51 @@
 <template>
-  <q-banner class="bg-grey-3">
-    <template v-slot:avatar>
-      <q-icon name="account_circle" color="primary" />
-    </template>
-    Register to access your todos anywhere!
-  </q-banner>
+  <q-form>
+    <div class="row q-mb-md">
+      <q-banner class="bg-grey-3 col">
+        <template v-slot:avatar>
+          <q-icon name="account_circle" color="primary" />
+        </template>
+        Register to access your todos anywhere!
+      </q-banner>
+    </div>
+
+    <div class="row q-mb-md">
+      <q-input
+        v-model="formData.email"
+        type="email"
+        outlined
+        class="col"
+        label="Email"
+        stack-label />
+    </div>
+
+    <div class="row q-mb-md">
+      <q-input
+        v-model="formData.password"
+        type="password"
+        outlined
+        class="col"
+        label="Password"
+        stack-label />
+    </div>
+
+    <div class="row">
+      <q-space />
+      <q-btn color="primary" label="Register" />
+    </div>
+  </q-form>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      formData: {
+        email: '',
+        password: '',
+      }
+    }
+  }
 };
 </script>
 
