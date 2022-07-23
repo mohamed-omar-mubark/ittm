c5336ded00694601ae63b994f480cb70d0aaddca
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2022-07-23 21:33:05 +0200

    feat: Read data when user logs in

--- src/store/store-auth.js
+++ src/store/store-auth.js
@@ -36,13 +36,14 @@ const actions = {
   logoutUser () {
     signOut(firebaseAuth)
   },
-  handleAuthStateChanged ({ commit }) {
+  handleAuthStateChanged ({ commit, dispatch }) {
     firebaseAuth.onAuthStateChanged(user => {
       Loading.hide()
       if (user) {
         commit('setLoggedIn', true)
         LocalStorage.set('loggedIn', true)
         this.$router.push('/').catch(err => {})
+        dispatch('tasks/fbReadData', null, { root: true })
       } else {
         commit('setLoggedIn', false)
         LocalStorage.set('loggedIn', false)
